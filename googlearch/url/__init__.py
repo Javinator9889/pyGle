3c7071b7d3bc541f2e48861a34f846d381d7f8c8
Javinator9889 <javialonso007@hotmail.es>
2018-07-05 17:14:47 +0200

    Included URL formatting - project created and refactored

---
+++ googlearch/url/__init__.py
@@ -0,0 +1,7 @@
+#
+#                py-google-search  Copyright (C) 2018  Javinator9889
+#   This program comes with ABSOLUTELY NO WARRANTY; for details add the "-h" option.
+#           This is free software, and you are welcome to redistribute it
+#                 under certain conditions; type "-L" for details.
+#
+
---
+++ googlearch/version.py
@@ -0,0 +1,15 @@
+#
+#                py-google-search  Copyright (C) 2018  Javinator9889
+#   This program comes with ABSOLUTELY NO WARRANTY; for details add the "-h" option.
+#           This is free software, and you are welcome to redistribute it
+#                 under certain conditions; type "-L" for details.
+#
+
+__version__ = "v0.1d"
+
+
+def print_ver_info():
+    import sys
+
+    print("py-google-search {0}".format(__version__))
+    print("Python {0}".format(sys.version.replace("\n", ' ')))
